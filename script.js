6ca22c0a24671856c372547d2773b95b80be80f7
Aishwary Kumar <aishwary.k@codeparva.in>
2023-11-28 14:54:50 +0530

    Updated argument

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // Function to be executed on button click
-function updateOutput() {
+function updateOutput(test3, test4) {
   // Get the output element
   var outputElement = document.getElementById('output');
 
